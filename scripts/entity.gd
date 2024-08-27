d54100e4ff8bd0b9e85735a66b6f501cf898390b
Ombarus <ombarus.dev@gmail.com>
2024-08-26 23:24:47 -0400

    initial commit: Basic Entity with a few actions prototype. Currently thinking about how to structure items usable in actions

---
+++ scripts/entity.gd
@@ -0,0 +1,132 @@
+extends Node2D
+
+enum STATE {
+	RUNNING,
+	FINISHED,
+	ERROR
+}
+
+@export var MovePixSec : float = 100.0
+@export var EatUnitSec : float = 0.3334
+@export var CookUnitSec : float = 0.3334
+@export var SleepEnerSec : float = 0.1
+
+@export var Sasiety : float = 0.0
+@export var Energy : float = 0.0
+
+var curParam : Dictionary
+var actionStack : Array
+var lastAction : String
+var lastState : int
+
+func _ready() -> void:
+	var target := Vector2(100, 100)
+	self.curParam = {"target": target}
+	self.actionStack = ["Goto"]
+
+func _process(delta: float) -> void:
+	var cur_action = self.getCurAction()
+	var s = self.callv(cur_action, [delta, self.curParam])
+	if (s == STATE.FINISHED):
+		self.popAction()
+	
+	self.lastState = s
+	self.modulate.r = self.Sasiety
+	self.modulate.g = self.Energy
+
+func Goto(delta : float, param : Dictionary) -> int:
+	var cur_pos := self.position
+	var dir := (param["target"] as Vector2) - cur_pos
+	var dir_norm = dir.normalized()
+	var move : Vector2 = dir_norm * self.MovePixSec * delta
+	var ret_val = STATE.RUNNING
+	if move.length_squared() > dir.length_squared() or move.length_squared() <= 0.0001:
+		move = dir
+		ret_val = STATE.FINISHED
+	self.position += move
+	return ret_val
+
+func Eat(delta : float, param : Dictionary) -> int:
+	var ret_val = STATE.RUNNING
+	var food_left := param["food"] as float
+	var eat := self.EatUnitSec * delta
+	if eat > food_left:
+		eat = food_left
+		ret_val = STATE.FINISHED
+	param["food"] = food_left - eat
+	self.Sasiety = clamp(self.Sasiety + eat, 0.0, 1.0)
+	
+	return ret_val
+	
+func Cook(delta : float, param : Dictionary) -> int:
+	if param.get("scene", "") != "":
+		return STATE.FINISHED
+		
+	var ret_val = STATE.RUNNING
+	var cook_left := param["meal"] as float
+	var progress := self.CookUnitSec * delta
+	if progress > cook_left:
+		progress = cook_left
+		param["scene"] = "res://scenes/entity.tscn"
+		param["pos"] = self.position
+		self.pushAction("Spawn")
+	param["meal"] = cook_left - progress
+	return ret_val
+
+func Default(delta : float, param : Dictionary) -> int:
+	var ret_val := STATE.RUNNING
+	if self.Energy <= 0.001:
+		self.pushAction("Sleep")
+		param["wake"] = randf_range(0.90, 1.00)
+	elif self.Sasiety <= 0.001 and not self.position.is_equal_approx(Vector2(150, 0)):
+		self.pushAction("Goto")
+		param["target"] = Vector2(150, 0)
+	elif self.Sasiety <= 0.001 and self.position.is_equal_approx(Vector2(150, 0)):
+		self.pushAction("Cook")
+		param["meal"] = 1.0
+	return ret_val
+
+func Spawn(delta : float, param : Dictionary) -> int:
+	var ret_val := STATE.RUNNING
+	var scene_name : String = param["scene"] as String
+	var pos : Vector2 = param["pos"] as Vector2
+	var pack := load(scene_name) as PackedScene
+	var n : Node2D = pack.instantiate() as Node2D
+	self.get_parent().add_child(n)
+	n.position = pos
+	ret_val = STATE.FINISHED
+	return ret_val
+
+func Pickup(delta : float, param : Dictionary) -> int:
+	var item : Node2D = param["item"] as Node2D
+	param.erase("item")
+	var inv : Array = param.get("inventory", [])
+	inv.push_back(item.name)
+	param["inventory"] = inv
+	item.queue_free()
+	return STATE.FINISHED
+	
+func Sleep(delta : float, param : Dictionary) -> int:
+	var ret_val := STATE.RUNNING
+	var recover := self.SleepEnerSec * delta
+	var wake_ener : float = param["wake"] as float
+	if self.Energy + recover > wake_ener:
+		self.Energy = wake_ener
+		return STATE.FINISHED
+	self.Energy += recover
+	return ret_val
+
+func pushAction(action : String) -> void:
+	print("Entity %s Pushed %s" % [self.name, action])
+	self.actionStack.push_back(action)
+	
+func popAction() -> void:
+	var last_action = self.actionStack[-1]
+	print("Entity %s Poped %s" % [self.name, last_action])
+	self.lastAction = last_action
+	self.actionStack.remove_at(len(self.actionStack)-1)
+	
+func getCurAction() -> String:
+	if len(self.actionStack) <= 0:
+		return "Default"
+	return self.actionStack[-1]
